68008c16a06e4756f2f18c16c5b39ee96ebda789
Ewan <ewan.s@cucumberwifi.io>
2020-03-03 16:02:57 +0000

    refactor for pins 0-3

--- src/main/java/com/codurance/bowling_calculator/BowlingCalculator.java
+++ src/main/java/com/codurance/bowling_calculator/BowlingCalculator.java
@@ -2,9 +2,7 @@ package com.codurance.bowling_calculator;
 
 public class BowlingCalculator {
     public int calculateScore(String pinsScored) {
-        if (pinsScored.equals("3")) return 3;
-        if (pinsScored.equals("2")) return 2;
-        if (pinsScored.equals("1")) return 1;
-        return 0;
+        if (pinsScored.equals("-")) return 0;
+        return Integer.parseInt(pinsScored);
     }
 }
